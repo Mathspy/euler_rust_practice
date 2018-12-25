5caaa8945f6f0818a6c8442bac97f8a221d22213
Mathspy <mathspy257@gmail.com>
2018-12-25 21:34:34 +0200

    Solved 003

--- 003_largest_prime_factor/src/main.rs
+++ 003_largest_prime_factor/src/main.rs
@@ -1,3 +1,41 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    println!(
+        "{}",
+        solve(args[1].parse().expect("Please input a valid integer"))
+    );
+}
+
+// Algorithm credit:
+// https://people.revoledu.com/kardi/tutorial/BasicMath/Prime/Algorithm-PrimeFactor.html
+fn solve(mut num: u64) -> u64 {
+    let mut prime = 2;
+    let mut solution = vec![];
+
+    while num >= prime * prime {
+        if num % prime == 0 {
+            solution.push(prime);
+            num = num / prime;
+            prime = 2;
+        } else {
+            prime = prime + 1;
+        }
+    }
+
+    solution.push(num);
+
+    return solution.into_iter().max().unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn produces_correct_answer() {
+        assert_eq!(6857, solve(600851475143));
+    }
 }
