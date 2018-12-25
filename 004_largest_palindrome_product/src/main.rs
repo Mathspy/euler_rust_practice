c98d223637eab7424e2c113ca62a48af2535a165
Mathspy <mathspy257@gmail.com>
2018-12-25 22:27:19 +0200

    Solved 004

--- 004_largest_palindrome_product/src/main.rs
+++ 004_largest_palindrome_product/src/main.rs
@@ -1,3 +1,53 @@
+#[macro_use]
+extern crate itertools;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", solve(1000));
+}
+
+fn solve(upper_bound: u32) -> u32 {
+    iproduct!(0..upper_bound, 0..upper_bound)
+        .map(|(x, y)| x * y)
+        .filter(|&product| is_palindrome(product))
+        .max()
+        .unwrap()
+}
+
+// Algorithm credit:
+// https://www.geeksforgeeks.org/check-number-palindrome-not-without-using-extra-space/
+fn is_palindrome(mut num: u32) -> bool {
+    let mut divisior = 1;
+    while num / divisior >= 10 {
+        divisior = divisior * 10;
+    }
+
+    while num != 0 {
+        let leading = num / divisior;
+        let trailing = num % 10;
+
+        if leading != trailing {
+            return false;
+        }
+
+        num = (num % divisior) / 10;
+
+        divisior = divisior / 100;
+    }
+
+    return true;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn produces_correct_answer_for_example() {
+        assert_eq!(9009, solve(100));
+    }
+
+    #[test]
+    fn produces_correct_answer_for_problem() {
+        assert_eq!(906609, solve(1000));
+    }
 }
