b862416731ac4db918f2c27a2c10fd3eeebb7ded
Mathspy <mathspy257@gmail.com>
2018-12-25 20:22:06 +0200

    Extracted 001's solution and tested it

--- 001_multiples_of_3_and_5/src/main.rs
+++ 001_multiples_of_3_and_5/src/main.rs
@@ -1,11 +1,23 @@
 fn main() {
-    let result = (0..1000).into_iter().fold(0, |acc, x| {
+    println!("{}", solve());
+}
+
+fn solve() -> i32 {
+    return (0..1000).into_iter().fold(0, |acc, x| {
         if x % 3 == 0 || x % 5 == 0 {
             acc + x
         } else {
             acc
         }
     });
+}
 
-    println!("{}", result);
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn produces_correct_answer() {
+        assert_eq!(233168, solve());
+    }
 }
