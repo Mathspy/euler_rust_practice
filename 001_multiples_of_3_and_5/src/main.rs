d4396f354e8f2df30095b5ff042534a288efbcf5
Mathspy <mathspy257@gmail.com>
2018-12-26 00:52:51 +0200

    Updated 001 and 002 with new modular format
    - Added extra tests for both 001 and 002 too

--- 001_multiples_of_3_and_5/src/main.rs
+++ 001_multiples_of_3_and_5/src/main.rs
@@ -1,9 +1,19 @@
+use std::env;
+
 fn main() {
-    println!("{}", solve());
+    let args: Vec<String> = env::args().collect();
+
+    println!(
+        "{}",
+        solve(args[1].parse().expect("Please input a valid integer"))
+    );
 }
 
-fn solve() -> i32 {
-    return (0..1000).into_iter().filter(|x| x % 3 == 0 || x % 5 == 0 ).sum();
+fn solve(upperbound: i32) -> i32 {
+    return (0..upperbound)
+        .into_iter()
+        .filter(|x| x % 3 == 0 || x % 5 == 0)
+        .sum();
 }
 
 #[cfg(test)]
@@ -11,7 +21,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn produces_correct_answer() {
-        assert_eq!(233168, solve());
+    fn produces_correct_answer_for_example() {
+        assert_eq!(23, solve(10));
+    }
+
+    #[test]
+    fn produces_correct_answer_for_problem() {
+        assert_eq!(233168, solve(1000));
     }
 }
--- 002_even_fibonacci_numbers/src/main.rs
+++ 002_even_fibonacci_numbers/src/main.rs
@@ -1,13 +1,20 @@
+use std::env;
+
 fn main() {
-    println!("{}", solve());
+    let args: Vec<String> = env::args().collect();
+
+    println!(
+        "{}",
+        solve(args[1].parse().expect("Please input a valid integer"))
+    );
 }
 
-fn solve() -> i32 {
+fn solve(upperbound: i32) -> i32 {
     let mut fib = vec![1, 2];
 
     loop {
         let new = fib.iter().rev().take(2).sum();
-        if new >= 4_000_000 {
+        if new >= upperbound {
             break;
         }
         fib.push(new);
@@ -21,7 +28,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn produces_correct_answer() {
-        assert_eq!(4613732, solve());
+    fn produces_correct_answer_for_example() {
+        assert_eq!(44, solve(100));
+    }
+
+    #[test]
+    fn produces_correct_answer_for_problem() {
+        assert_eq!(4613732, solve(4_000_000));
     }
 }
