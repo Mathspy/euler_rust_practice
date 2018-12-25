a6f834b565a66b201f32896239cec5066ce3e4cf
Mathspy <mathspy257@gmail.com>
2018-12-25 21:06:01 +0200

    Improved solution for 001 with knowledge from 002 research .sum() rocks

--- 001_multiples_of_3_and_5/src/main.rs
+++ 001_multiples_of_3_and_5/src/main.rs
@@ -3,13 +3,7 @@ fn main() {
 }
 
 fn solve() -> i32 {
-    return (0..1000).into_iter().fold(0, |acc, x| {
-        if x % 3 == 0 || x % 5 == 0 {
-            acc + x
-        } else {
-            acc
-        }
-    });
+    return (0..1000).into_iter().filter(|x| x % 3 == 0 || x % 5 == 0 ).sum();
 }
 
 #[cfg(test)]
