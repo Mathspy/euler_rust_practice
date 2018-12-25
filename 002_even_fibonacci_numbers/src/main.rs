60e6d48cc792554e31cf7ae3c90ed79b0b74b1c5
Mathspy <mathspy257@gmail.com>
2018-12-25 20:41:53 +0200

    Solved 002

--- 002_even_fibonacci_numbers/src/main.rs
+++ 002_even_fibonacci_numbers/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", solve());
+}
+
+fn solve() -> i32 {
+    let mut num1 = 1;
+    let mut num2 = 2;
+
+    let mut sum = num2;
+    while num1 + num2 < 4_000_000 {
+        let new = num1 + num2;
+        if new % 2 == 0 {
+            sum += new;
+        }
+
+        num1 = num2;
+        num2 = new;
+    }
+
+    return sum;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn produces_correct_answer() {
+        assert_eq!(4613732, solve());
+    }
 }
