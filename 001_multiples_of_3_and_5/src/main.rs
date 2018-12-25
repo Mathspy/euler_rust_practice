559ea1e3e8b925f4995385815636879427c08b7f
Mathspy <mathspy257@gmail.com>
2018-12-25 20:21:29 +0200

    Solved 001

--- 001_multiples_of_3_and_5/src/main.rs
+++ 001_multiples_of_3_and_5/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let result = (0..1000).into_iter().fold(0, |acc, x| {
+        if x % 3 == 0 || x % 5 == 0 {
+            acc + x
+        } else {
+            acc
+        }
+    });
+
+    println!("{}", result);
 }
