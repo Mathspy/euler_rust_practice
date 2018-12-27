9ab742139cbd810e2af89a4a76e9a34d1b76b017
Mathspy <mathspy257@gmail.com>
2018-12-27 14:34:33 +0200

    Solved 006

--- 006_sum_square_difference/src/main.rs
+++ 006_sum_square_difference/src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    println!(
+        "{}",
+        solve(args[1].parse().expect("Please input a valid integer"))
+    );
+}
+
+fn solve(amount: u32) -> u64 {
+    let sum_of_squares: u32 = (1..=amount).into_iter().map(|x| x * x).sum();
+
+    let sum_of_values: u32 = (1..=amount).into_iter().sum();
+    let square_of_sum = sum_of_values * sum_of_values;
+
+    return square_of_sum as u64 - sum_of_squares as u64;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn produces_correct_answer_for_example() {
+        assert_eq!(2640, solve(10));
+    }
+
+    #[test]
+    fn produces_correct_answer_for_problem() {
+        assert_eq!(25164150, solve(100));
+    }
 }
